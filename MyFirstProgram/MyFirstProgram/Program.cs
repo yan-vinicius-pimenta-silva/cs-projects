7ead7835f0f1674b082f43bcd324f4012b6bc4f5
yan-vinicius-pimenta-silva <yan.fedora@gmail.com>
2025-01-31 13:12:30 -0300

    first program in cs

---
+++ MyFirstProgram/MyFirstProgram/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+
+        static void Main(string[]args)
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("It's really good");
+         //Console.Beep(); //Beeps when it reaches the end
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+}
